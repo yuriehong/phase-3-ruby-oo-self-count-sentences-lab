44e551dc29cc8b4affad921c3064bf7d8a69c2bd
+ <+>
2022-09-01 08:47:26 -0400

    commit tree 6b0c6dd86fc5a5fb3668e7e7afb82ea967ae4b95

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
+
 
   end
 
   def exclamation?
+    self.end_with?("!")
+
 
   end
 
   def count_sentences
+    count = 0
+    split = self.split(".")
+    return split.count
 
   end
 end
